f1b0bdb93005758255c0049a3a513c9bd2b188d7
Konstantin Konstantin <zetslief@protonmail.com>
2023-12-15 16:50:06 +0100

    Update readme and main.js output format.

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 Simple calculator built for educational purposes.
 
+Run calculator:
+```bash
+node main.js 1 + 1
+```
+
 Run tests:
 ```bash
 npm test
--- main.js
+++ main.js
@@ -3,9 +3,8 @@ import { buildExpression, calculateAst, drawAst } from "./calc.js";
 function main(expression) {
   const ast = buildExpression(expression);
   drawAst(ast);
-  console.log("Result", calculateAst(ast));
+  console.log(expression, "=", calculateAst(ast));
 }
 
 const expression = "".concat(...process.argv.slice(2));
-console.log(expression);
 main(expression);
